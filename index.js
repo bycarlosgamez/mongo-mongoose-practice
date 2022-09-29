60212ed2e21c0ed8f82e5c1e7931830bea95bdd2
Carlos Gámez <hardpx@gmail.com>
2022-09-29 11:06:41 -0500

    created a model added a document to mongo

--- index.js
+++ index.js
@@ -8,3 +8,19 @@ mongoose
   .catch((err) => {
     console.log(`Conection error: ${err}`);
   });
+
+const pedalSchema = new mongoose.Schema({
+  name: String,
+  brand: String,
+  knobs: Number,
+  type: String,
+});
+
+const Pedal = mongoose.model("Pedal", pedalSchema);
+
+const smallStone = new Pedal({
+  name: "Small Stone",
+  brand: "Electro Harmonix",
+  knobs: 1,
+  type: "phaser",
+});
